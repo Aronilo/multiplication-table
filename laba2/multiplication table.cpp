4aa1e83546faf68ee85d988a6b35a23ab52e14ae
Валерия Каширина ПК <Aronilo@mail.ru>
2021-10-10 21:18:39 +0300

    тык

--- laba2/multiplication table.cpp
+++ laba2/multiplication table.cpp
@@ -1,16 +1,21 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 int main()
 {
-    for (int i = 1; i < 10; i++)
+    for (int k = 0; k <= 1; k++)
     {
-        for (int j = 1; j < 10; j++)
+        for (int i = 2; i <= 10; i++)
         {
-            cout << i << "*" << j << "=" << i * j << endl;
+            for (int j = 1; j <= 4; j++)
+            {
+                cout << setw(2) << j + k * 4 + 1 << " X " << setw(2) << i << " = " << setw(2) << (j + k * 4 + 1) * i << "     ";
 
-            cout << i << "*" << j << "=" << i * j << endl;
+            }
+            cout << endl;
         }
+        cout << endl;
     }
     system("pause");
 }
